cf6377752580a8419b89d488e65651e1a3fe024d
MacApos <maciejapostol98@gmail.com>
2026-02-19 18:33:14 +0100

    add FolderImpl and correct FileCabinet methods

--- src/main/java/com/FileCabinet.java
+++ src/main/java/com/FileCabinet.java
@@ -8,62 +8,41 @@ public class FileCabinet implements Cabinet {
     private List<Folder> folders;
 
     public FileCabinet(List<Folder> folders) {
-        this.folders = new ArrayList<>();
+        this.folders = new ArrayList<>(folders);
     }
 
     public List<Folder> getFolders() {
         return folders;
     }
 
-    public void addFolder(Folder folder) {
-        assert folder != null;
-        folders.add(folder);
-    }
-
-    public void removeFolder(Folder folder) {
-        assert folder != null;
-        folders.remove(folder);
-    }
-
-    private void sortMultiFolderLast(List<Folder> folders) {
-        folders.sort(Comparator.comparing(folder -> folder instanceof MultiFolder ? 0 : 1));
-    }
-
-    private void recursion(List<Folder> folders) {
-        sortMultiFolderLast(folders);
-        for (Folder folder : folders) {
-            System.out.println(folder.getName());
-            if (folder instanceof MultiFolder multiFolder) {
-                recursion(multiFolder.getFolders());
-            }
-        }
-    }
-
-    private <T> Stream<Folder> filterBy(Function<Folder, T> extractionFunction, String field) {
-        folders.sort(Comparator.comparing(folder -> folder instanceof MultiFolder ? 0 : 1));
-
-        for (Folder folder : folders) {
-            if (folder instanceof MultiFolder) {
-
-            }
-        }
-
+    private <T> Stream<Folder> filterBy(List<Folder> folders, Function<Folder, T> extractionFunction, String field) {
         return folders.stream().filter(f -> Objects.equals(extractionFunction.apply(f), field));
+    }
+
+    public ArrayList<Folder> recursivePass(List<Folder> folders, ArrayList<Folder> flatFolders) {
+        for (Folder folder : folders) {
+            flatFolders.add(folder);
+            if (folder instanceof MultiFolder multiFolder) {
+                recursivePass(multiFolder.getFolders(), flatFolders);
+            }
+        }
+        return flatFolders;
     }
 
     @Override
     public Optional<Folder> findFolderByName(String name) {
-        recursion(folders);
-        return filterBy(Folder::getName, name).findFirst();
+        return filterBy(folders, Folder::getName, name).findFirst();
     }
 
     @Override
     public List<Folder> findFoldersBySize(String size) {
-        return filterBy(Folder::getSize, size).toList();
+        Size.checkSize(size);
+        ArrayList<Folder> foldersBySize = recursivePass(folders, new ArrayList<>());
+        return filterBy(foldersBySize, Folder::getSize, size).toList();
     }
 
     @Override
     public int count() {
-        return folders.size();
+        return  recursivePass(folders, new ArrayList<>()).size();
     }
 }
---
+++ src/main/java/com/Size.java
@@ -0,0 +1,15 @@
+package com;
+
+public enum Size {
+    SMALL,
+    MEDIUM,
+    LARGE;
+
+    public static void checkSize(String size) {
+        try {
+            Size.valueOf(size);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new IllegalArgumentException("Invalid size!");
+        }
+    }
+}
